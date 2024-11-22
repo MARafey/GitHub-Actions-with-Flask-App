f85d9cf8a980e5810c4567cb750b562089ffd178
Muhammad Abdur Rafey <m.a.rafey1215@gmail.com>
2024-11-22 23:09:33 +0500

    Testing workflow

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello,My name is Muhammad Abdur Rafey and this is my test for the Github Actions"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
